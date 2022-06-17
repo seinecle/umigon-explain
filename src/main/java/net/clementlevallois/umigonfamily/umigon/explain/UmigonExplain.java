6eaae1166cb827d6911798dd43b09fda9c5371ce
clement levallois <clementlevallois@protonmail.com>
2022-06-17 17:02:44 +0200

    progress on introducing explicit decision making for sentiment adjudication

---
+++ src/main/java/net/clementlevallois/umigonfamily/umigon/explain/UmigonExplain.java
@@ -0,0 +1,16 @@
+/*
+ * author: Clément Levallois
+ */
+
+package net.clementlevallois.umigonfamily.umigon.explain;
+
+/**
+ *
+ * @author LEVALLOIS
+ */
+public class UmigonExplain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
